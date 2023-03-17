5c2e9ad42d1aff60e40ed23e67af4544765a782c
Mohammad Zafar <mzcoder7@gmail.com>
2023-03-17 12:33:15 +0000

    main functionality (in progress)

--- main.js
+++ main.js
@@ -1,45 +1,52 @@
 //logic
 let moves = ['rock', 'paper', 'scissors'];
 
-let score = 0;
+let games = 0;
+let totalScore = 0;
 // console.log(playerMove);
 // console.log(computerMove);
 
-let playerMove = prompt('What is your move? rock, paper, or scissors ?');
+let startGame = confirm('Do you want to play the game?');
+while (startGame) {
+	let playerMove = prompt('What is your move? rock, paper, or scissors ?');
 
-let computerMove = 'paper';
+	function getWinner() {
+		let computerMove = moves[Math.floor(Math.random() * moves.length)];
+		let score = 0;
+		if (playerMove === 'rock') {
+			switch (computerMove) {
+				case 'scissors':
+					score = 1;
+					break;
+				case 'paper':
+					score = -1;
+			}
+			totalScore += score;
+		}
+		if (playerMove === 'scissors') {
+			switch (computerMove) {
+				case 'paper':
+					score = 1;
+					break;
+				case 'rock':
+					score = -1;
+			}
+			totalScore += score;
+		}
+		if (playerMove === 'paper') {
+			switch (computerMove) {
+				case 'rock':
+					score = 1;
+					break;
+				case 'scissors':
+					score = -1;
+			}
+			totalScore += score;
+		}
+		alert(`Current round score: ${score} Total Score: ${totalScore}`);
+	}
 
-function getWinner(playerMove, player2) {
-	let score = 0;
-	if (playerMove === 'rock') {
-		switch (player2) {
-			case 'scissors':
-				score = 1;
-				break;
-			case 'paper':
-				score = -1;
-		}
-	}
-	if (playerMove === 'scissors') {
-		switch (player2) {
-			case 'paper':
-				score = 1;
-				break;
-			case 'rock':
-				score = -1;
-		}
-	}
-	if (playerMove === 'paper') {
-		switch (player2) {
-			case 'rock':
-				score = 1;
-				break;
-			case 'scissors':
-				score = -1;
-		}
-	}
-
-	return score;
+	getWinner();
+	games++;
 }
-
-console.log(getWinner(playerMove, 'rock'));
+// console.log(getWinner());
